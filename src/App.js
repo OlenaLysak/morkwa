a578eaf4642d9b1f98ddba5f92d7a612b9789d57
OlenaLysak <olena.lysak@lexchain.ca>
2024-02-04 17:57:51 +0200

    inital

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+//Style
+import styles from './App.module.css';
+
+//Images
+import logo from './assets/logo.png';
+import pin from './assets/pin.png';
+import facebookIcon from './assets/facebook-icon.png';
+import instaIcon from './assets/insta-icon.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.App}>
+      <header className={styles.header}>
+        <div className={styles.logoContainer}>
+          <img alt="Morkwa" src={logo} className={styles.logoImg} />
+        </div>
       </header>
+      <div className={styles.content}>
+        <div className={styles.subtitle}>COMING SOON</div>
+        <div className={styles.products}>
+          <div className={styles.test}>Кастомізація під бренд</div>
+          <div>мішечки / шопери / набори</div>
+          <div>брендування продукції</div>
+        </div>
+        <div className={styles.announcement}>WEBSITE UNDER CONSTRUCTION</div>
+        <div className={styles.socialNetworks}>
+          <div>Follow us</div>
+          <div>
+            <img src={facebookIcon} alt="facebook" />
+          </div>
+          <div>
+            <img src={instaIcon} alt="instagram" />
+          </div>
+        </div>
+      </div>
+      <footer className={styles.footer}>
+        <div className={styles.pinIcon}>
+          <img src={pin} alt="pin" />
+        </div>
+        <div>м. Львів, вул. Зелена 115д, оф. 521</div>
+      </footer>
     </div>
   );
 }
